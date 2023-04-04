d583dfa5b762599c21600fa65ff69fe70e742088
tsuen4 <etshinnaga@gmail.com>
2023-04-04 09:50:24 +0900

    フォーマッタ変更

--- putter/functions/putter/action.ts
+++ putter/functions/putter/action.ts
@@ -1,98 +1,90 @@
-import {Client} from '@notionhq/client';
+import { Client } from '@notionhq/client';
 import {
-  AppendBlockChildrenResponse,
-  BlockObjectRequest,
-  PageObjectResponse,
+    AppendBlockChildrenResponse,
+    BlockObjectRequest,
+    PageObjectResponse,
 } from '@notionhq/client/build/src/api-endpoints';
 
 export class Action {
-  private client: Client;
+    private client: Client;
 
-  constructor(token: string, private readonly databaseId: string) {
-    this.client = new Client({
-      auth: token,
-    });
-  }
-
-  #generateChildren(text: string): BlockObjectRequest {
-    return {
-      paragraph: {
-        rich_text: [
-          {
-            type: 'text',
-            text: {
-              content: text,
-            },
-          },
-        ],
-      },
-    };
-  }
-
-  async #getPageByTitle(
-    title: string
-  ): Promise<PageObjectResponse | undefined> {
-    const response = await this.client.databases.query({
-      database_id: this.databaseId,
-      filter: {
-        property: 'title',
-        title: {
-          equals: title,
-        },
-      },
-    });
-    return response.results
-      .filter((result): result is PageObjectResponse => {
-        const parent = (result as PageObjectResponse).parent;
-        if (parent.type !== 'database_id') {
-          return false;
-        }
-        return parent.database_id.split('-').join('') === this.databaseId;
-      })
-      .at(0);
-  }
-
-  async #createPageWithText(
-    title: string,
-    text: string
-  ): Promise<PageObjectResponse> {
-    return this.client.pages
-      .create({
-        parent: {
-          database_id: this.databaseId,
-        },
-        properties: {
-          title: [
-            {
-              text: {
-                content: title,
-              },
-            },
-          ],
-        },
-        children: [this.#generateChildren(text)],
-      })
-      .then(response => response as PageObjectResponse);
-  }
-
-  async #appendText(
-    blockId: string,
-    text: string
-  ): Promise<AppendBlockChildrenResponse> {
-    return this.client.blocks.children
-      .append({
-        block_id: blockId,
-        children: [this.#generateChildren(text)],
-      })
-      .then(response => response as AppendBlockChildrenResponse);
-  }
-
-  async put(title: string, text: string): Promise<void> {
-    const existPage = await this.#getPageByTitle(title);
-    if (existPage !== undefined) {
-      await this.#appendText(existPage.id, text);
-    } else {
-      await this.#createPageWithText(title, text);
+    constructor(token: string, private readonly databaseId: string) {
+        this.client = new Client({
+            auth: token,
+        });
     }
-  }
+
+    #generateChildren(text: string): BlockObjectRequest {
+        return {
+            paragraph: {
+                rich_text: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: text,
+                        },
+                    },
+                ],
+            },
+        };
+    }
+
+    async #getPageByTitle(title: string): Promise<PageObjectResponse | undefined> {
+        const response = await this.client.databases.query({
+            database_id: this.databaseId,
+            filter: {
+                property: 'title',
+                title: {
+                    equals: title,
+                },
+            },
+        });
+        return response.results
+            .filter((result): result is PageObjectResponse => {
+                const parent = (result as PageObjectResponse).parent;
+                if (parent.type !== 'database_id') {
+                    return false;
+                }
+                return parent.database_id.split('-').join('') === this.databaseId;
+            })
+            .at(0);
+    }
+
+    async #createPageWithText(title: string, text: string): Promise<PageObjectResponse> {
+        return this.client.pages
+            .create({
+                parent: {
+                    database_id: this.databaseId,
+                },
+                properties: {
+                    title: [
+                        {
+                            text: {
+                                content: title,
+                            },
+                        },
+                    ],
+                },
+                children: [this.#generateChildren(text)],
+            })
+            .then((response) => response as PageObjectResponse);
+    }
+
+    async #appendText(blockId: string, text: string): Promise<AppendBlockChildrenResponse> {
+        return this.client.blocks.children
+            .append({
+                block_id: blockId,
+                children: [this.#generateChildren(text)],
+            })
+            .then((response) => response as AppendBlockChildrenResponse);
+    }
+
+    async put(title: string, text: string): Promise<void> {
+        const existPage = await this.#getPageByTitle(title);
+        if (existPage !== undefined) {
+            await this.#appendText(existPage.id, text);
+        } else {
+            await this.#createPageWithText(title, text);
+        }
+    }
 }
